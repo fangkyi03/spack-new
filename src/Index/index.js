5502a634218da0e54b2d76c5695ec650513b163a
fangkyi03 <fangkyi02@live.cn>
2020-10-26 09:46:21 +0800

    1.修改

--- src/Index/index.js
+++ src/Index/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import './index.less'
-import A from './a'
 import { Button, Radio } from 'antd'
 export default function Index() {
   const [text,setText] = React.useState(Math.random() * 1000)
   return (
     <div onClick={()=>setText(Math.random() * 1000)}>
-      <A text={text}></A>
+      测试
     </div>
   )
 }
