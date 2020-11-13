5e2e69fd95ac8b03b3e6806b750cd1d1f0f67440
fangkyi03 <fangkyi02@live.cn>
2020-11-13 22:08:36 +0800

    1.修改

---
+++ src/pages/Index/index.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    data() {
+        return {
+            inputValue:''
+        }
+    },
+    mounted() {
+        vant.Notify('测试')
+    },
+    methods: {
+        renderInput() {
+            return (
+                <input value={this.inputValue}/>
+            )
+        }
+    },
+    render(h) {
+        return (
+            <div>
+                <van-search v-model="value" placeholder="请输入搜索关键词" />
+                <van-button>ceshi</van-button>
+                <div onClick={()=>this.inputValue = Math.random()}>点击给编辑框赋值</div>
+                {this.renderInput()}
+            </div>
+        )
+    },
+}
+</script>
