3a774473b5b2c3793d9a9e8e62127d08dfb0b891
fangkyi02 <fangkyi02>
2021-12-24 14:05:57 +0800

    1.增加缺失依赖文件

--- src/pages/Index/index.vue
+++ src/pages/Index/index.vue
@@ -1,29 +1,21 @@
 <script>
 export default {
-    data() {
-        return {
-            inputValue:''
-        }
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    renderInput() {
+      return <input value={this.inputValue} />;
     },
-    mounted() {
-        vant.Notify('测试')
-    },
-    methods: {
-        renderInput() {
-            return (
-                <input value={this.inputValue}/>
-            )
-        }
-    },
-    render(h) {
-        return (
-            <div>
-                <van-search v-model="value" placeholder="请输入搜索关键词" />
-                <van-button>ceshi</van-button>
-                <div onClick={()=>this.inputValue = Math.random()}>点击给编辑框赋值</div>
-                {this.renderInput()}
-            </div>
-        )
-    },
-}
+  },
+  render(h) {
+    return (
+      <div>
+        <van-button>测试</van-button>
+      </div>
+    );
+  },
+};
 </script>
